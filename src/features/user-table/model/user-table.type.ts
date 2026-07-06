b99b60421f0e7e55c6567338cb4af8f0e76622e0
AlexisRoe <a.roehrling@gmx.de>
2026-07-06 14:10:31 +0200

    Delete unused code

--- src/features/user-table/model/user-table.type.ts
+++
@@ -1,7 +0,0 @@
-import type { User } from "@entities/user/model/user.type";
-
-export interface UserTableState {
-  users: User[];
-  isLoading: boolean;
-  error: string | null;
-}
--- src/shared/ui/data-table/data-table.component.tsx
+++
@@ -1,6 +0,0 @@
-import type { DataTableProps } from "mantine-datatable";
-import { DataTable as MantineDataTable } from "mantine-datatable";
-
-export function DataTable<T>(props: DataTableProps<T>) {
-  return <MantineDataTable {...props} />;
-}
